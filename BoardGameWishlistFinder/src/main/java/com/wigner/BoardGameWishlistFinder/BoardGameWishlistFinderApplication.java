93d3b68a7daf3e3e7e6f062360f1f9f0dd2c78f6
WignerSantos <wigner12@gmail.com>
2023-09-12 21:58:02 -0300

    Initial project

---
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/BoardGameWishlistFinderApplication.java
@@ -0,0 +1,13 @@
+package com.wigner.BoardGameWishlistFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameWishlistFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameWishlistFinderApplication.class, args);
+	}
+
+}
