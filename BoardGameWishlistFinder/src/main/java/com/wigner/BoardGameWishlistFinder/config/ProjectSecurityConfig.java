cc71fcb7547cb43eb8571b88ac772c8950a19630
WignerSantos <wigner12@gmail.com>
2023-09-13 16:52:03 -0300

    Added ProjectSecurityConfig
    Configured Matchers /login /assets/** /dashboard(not exist)

--- BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/config/ProjectSecurityConfig.java
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/config/ProjectSecurityConfig.java
@@ -1,2 +1,25 @@
-package com.wigner.BoardGameWishlistFinder.config;public class ProjectSecurityConfig {
+package com.wigner.BoardGameWishlistFinder.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.ignoringRequestMatchers(""))
+                .authorizeHttpRequests((request) -> request.requestMatchers("/login").permitAll()
+                        .requestMatchers("/assets/**").permitAll()
+                        .requestMatchers("/dashboard").permitAll())
+                        .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
+                                .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true"))
+                        .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
 }
