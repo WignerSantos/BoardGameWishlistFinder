464c5455c9a1b3c1061db31173d50b5361f5a6d4
WignerSantos <wigner12@gmail.com>
2023-09-13 16:50:20 -0300

    Added login controller
    DisplayLoginPage method added

--- BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/controller/LoginController.java
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/controller/LoginController.java
@@ -1,2 +1,28 @@
-package com.wigner.BoardGameWishlistFinder.controller;public class LoginController {
+package com.wigner.BoardGameWishlistFinder.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
+    public ModelAndView displayLoginPage(@RequestParam(value = "error", required = false) String error) {
+
+        ModelAndView modelAndView = new ModelAndView("login.html");
+
+        String errorMessage = null;
+
+        if (error != null) {
+            errorMessage = "Usuário ou senha está incorreto!";
+        }
+
+        modelAndView.addObject("errorMessage", errorMessage);
+
+        return modelAndView;
+    }
+
 }
