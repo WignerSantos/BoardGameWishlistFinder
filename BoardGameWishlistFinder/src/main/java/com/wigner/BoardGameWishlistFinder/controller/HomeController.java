57e2f024261d8d132b1cf120305b1f52c1cd65dc
WignerSantos <wigner12@gmail.com>
2023-09-15 15:23:21 -0300

    Added Home controller
    Added displayHomePage method

--- BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/controller/HomeController.java
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/controller/HomeController.java
@@ -1,2 +1,16 @@
-package com.wigner.BoardGameWishlistFinder.controller;public class HomeController {
+package com.wigner.BoardGameWishlistFinder.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public ModelAndView displayHomePage() {
+        return new ModelAndView("home.html");
+    }
+
 }
