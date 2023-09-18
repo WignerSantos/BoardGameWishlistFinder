2f072a1149c928b6cb566690bd6e11c4eb0b7868
WignerSantos <wigner12@gmail.com>
2023-09-18 15:50:12 -0300

    Add Password annotaion as validation constraints

--- BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/annotations/Password.java
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/annotations/Password.java
@@ -1,2 +1,21 @@
-package com.wigner.BoardGameWishlistFinder.annotations;public class Password {
+package com.wigner.BoardGameWishlistFinder.annotations;
+
+import com.wigner.BoardGameWishlistFinder.vallidations.PasswordValidation;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = PasswordValidation.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Password {
+
+    String message() default "Please choose a strong password!";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload> [] payload() default {};
+
 }
