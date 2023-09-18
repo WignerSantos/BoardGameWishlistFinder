b6ce3ff6d0467bf799a0ae27b441de427ee379e4
WignerSantos <wigner12@gmail.com>
2023-09-18 15:51:50 -0300

    Added Confirm values validator of ConfirmValuesMatch

--- BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/vallidations/ConfirmValuesValidator.java
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/vallidations/ConfirmValuesValidator.java
@@ -1,2 +1,33 @@
-package com.wigner.BoardGameWishlistFinder.vallidations;public class ConfirmValuesValidator {
+package com.wigner.BoardGameWishlistFinder.vallidations;
+
+import com.wigner.BoardGameWishlistFinder.annotations.ConfirmValuesMatch;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class ConfirmValuesValidator implements ConstraintValidator<ConfirmValuesMatch, Object> {
+
+    private String field;
+
+    private String fieldMatch;
+
+    @Override
+    public void initialize(ConfirmValuesMatch constraintAnnotation) {
+        this.field = constraintAnnotation.field();
+        this.fieldMatch = constraintAnnotation.fieldMatch();
+    }
+
+    @Override
+    public boolean isValid(Object obj, ConstraintValidatorContext constraintValidatorContext) {
+
+        Object fieldValue = new BeanWrapperImpl(obj).getPropertyValue(field);
+        Object fieldMatchValue = new BeanWrapperImpl(obj).getPropertyValue(fieldMatch);
+
+        if (fieldValue != null) {
+            return fieldValue.equals(fieldMatchValue);
+        } else {
+            return false;
+        }
+
+    }
 }
