bd3531092415fcf048eb7bf446a0fb5584d08074
WignerSantos <wigner12@gmail.com>
2023-09-18 15:52:30 -0300

    Added Password validation of password annotation

--- BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/vallidations/PasswordValidation.java
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/vallidations/PasswordValidation.java
@@ -1,2 +1,23 @@
-package com.wigner.BoardGameWishlistFinder.vallidations;public class PasswordValidation {
+package com.wigner.BoardGameWishlistFinder.vallidations;
+
+import com.wigner.BoardGameWishlistFinder.annotations.Password;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PasswordValidation implements ConstraintValidator<Password, String> {
+
+    List<String> weakPasswords;
+
+    @Override
+    public void initialize(Password constraintAnnotation) {
+        weakPasswords = Arrays.asList("123456789", "987654321", "000000000");
+    }
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
+        return password != null && (!weakPasswords.contains(password));
+    }
 }
