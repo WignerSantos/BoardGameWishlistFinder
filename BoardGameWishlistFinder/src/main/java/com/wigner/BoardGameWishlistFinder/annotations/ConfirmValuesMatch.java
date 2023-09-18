18b7160baeeb65111b07cbbdb0dc3afaaaf166c7
WignerSantos <wigner12@gmail.com>
2023-09-18 15:50:48 -0300

    Added ConfirmValuesMatch annotation as validation constraints

--- BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/annotations/ConfirmValuesMatch.java
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/annotations/ConfirmValuesMatch.java
@@ -1,2 +1,33 @@
-package com.wigner.BoardGameWishlistFinder.annotations;public @interface ConfirmValuesMatch {
+package com.wigner.BoardGameWishlistFinder.annotations;
+
+import com.wigner.BoardGameWishlistFinder.vallidations.ConfirmValuesValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = ConfirmValuesValidator.class)
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ConfirmValuesMatch {
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String message() default "Confirm values do not match!";
+
+    String field();
+
+    String fieldMatch();
+
+    @Target({ ElementType.TYPE })
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface list {
+        ConfirmValuesMatch[] value();
+    }
+
 }
