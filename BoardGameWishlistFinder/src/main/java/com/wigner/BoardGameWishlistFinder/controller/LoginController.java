f45ac0026cebba3647f3f482f42596d5f1e9e0fd
WignerSantos <wigner12@gmail.com>
2023-09-18 16:15:07 -0300

    Added logout method
    Added logout message when logging out

--- BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/controller/LoginController.java
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/controller/LoginController.java
@@ -1,5 +1,11 @@
 package com.wigner.BoardGameWishlistFinder.controller;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,7 +16,8 @@ import org.springframework.web.servlet.ModelAndView;
 public class LoginController {
 
     @RequestMapping(value = "/login", method = {RequestMethod.GET, RequestMethod.POST})
-    public ModelAndView displayLoginPage(@RequestParam(value = "error", required = false) String error) {
+    public ModelAndView displayLoginPage(@RequestParam(value = "error", required = false) String error,
+                                         @RequestParam(value = "logout", required = false) String logout) {
 
         ModelAndView modelAndView = new ModelAndView("login.html");
 
@@ -18,11 +25,24 @@ public class LoginController {
 
         if (error != null) {
             errorMessage = "Usuário ou senha está incorreto!";
+        } else if (logout != null) {
+            errorMessage = "Você foi desconectado com sucesso!";
         }
 
         modelAndView.addObject("errorMessage", errorMessage);
 
         return modelAndView;
+    }
+
+    @RequestMapping(value = "/logout" , method = RequestMethod.GET)
+    public ModelAndView logoutPage(HttpServletRequest request,
+                                   HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return new ModelAndView("redirect:/login?logout=true");
     }
 
 }
