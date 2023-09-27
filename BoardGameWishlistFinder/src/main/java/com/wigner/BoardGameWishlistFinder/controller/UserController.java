3dac7afee837fef8989e86fffec840a0c3927e93
WignerSantos <wigner12@gmail.com>
2023-09-27 13:06:20 -0300

    Adding displayCreateUser method

--- BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/controller/UserController.java
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/controller/UserController.java
@@ -2,6 +2,8 @@ package com.wigner.BoardGameWishlistFinder.controller;
 
 import com.wigner.BoardGameWishlistFinder.model.Person;
 import com.wigner.BoardGameWishlistFinder.model.Roles;
+import com.wigner.BoardGameWishlistFinder.repositories.RolesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +18,9 @@ import java.util.List;
 @Controller
 public class UserController {
 
+    @Autowired
+    RolesRepository rolesRepository;
+
     @RequestMapping(value = "/users", method = RequestMethod.GET)
     public ModelAndView displayUsersPage() {
 
@@ -25,6 +30,19 @@ public class UserController {
                 new Person(2, "Wigner2", "wigner13@gmail.com", "wigner13@gmail.com","123456789", "123456789", LocalDateTime.now(), "Wigner", LocalDateTime.now(), "", new Roles(1, "ADMIN")));
 
         modelAndView.addObject("users", users);
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/displayCreateUser", method = RequestMethod.GET)
+    public ModelAndView displayCreateUserPage() {
+
+        ModelAndView modelAndView = new ModelAndView("create_users.html");
+
+        List<Roles> roles = rolesRepository.findAll();
+
+        modelAndView.addObject("user", new Person());
+        modelAndView.addObject("roles", roles);
 
         return modelAndView;
     }
