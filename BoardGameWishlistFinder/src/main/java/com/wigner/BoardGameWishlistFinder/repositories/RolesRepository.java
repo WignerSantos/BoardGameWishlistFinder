f7bb888b0012f8329d2c795c4c528a07006ee4f3
WignerSantos <wigner12@gmail.com>
2023-09-27 13:05:49 -0300

    Adding Roles repository

--- BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/repositories/RolesRepository.java
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/repositories/RolesRepository.java
@@ -1,2 +1,12 @@
-package com.wigner.BoardGameWishlistFinder.repositories;public interface RolesRepository {
+package com.wigner.BoardGameWishlistFinder.repositories;
+
+import com.wigner.BoardGameWishlistFinder.model.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RolesRepository extends JpaRepository<Roles, Integer> {
+
+    Roles getRoleByRoleName(String roleName);
+
 }
