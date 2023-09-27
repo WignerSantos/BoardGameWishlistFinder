6721122d99ce6b0169f443b3982f9cb770916c8a
WignerSantos <wigner12@gmail.com>
2023-09-27 13:20:22 -0300

    Enabling jpa repositories
    Adding EntityScan

--- BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/BoardGameWishlistFinderApplication.java
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/BoardGameWishlistFinderApplication.java
@@ -2,8 +2,12 @@ package com.wigner.BoardGameWishlistFinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.wigner.BoardGameWishlistFinder.repositories")
+@EntityScan("com.wigner.BoardGameWishlistFinder.model")
 public class BoardGameWishlistFinderApplication {
 
 	public static void main(String[] args) {
