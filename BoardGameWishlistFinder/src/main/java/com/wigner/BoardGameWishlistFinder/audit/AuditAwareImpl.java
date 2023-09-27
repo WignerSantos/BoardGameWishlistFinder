d521dc70381266a3500ebc1d27ea04909a4d3b1c
WignerSantos <wigner12@gmail.com>
2023-09-27 15:51:50 -0300

    Adding auditAwareImpl to get current auditor

--- BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/audit/AuditAwareImpl.java
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/audit/AuditAwareImpl.java
@@ -1,2 +1,17 @@
-package com.wigner.BoardGameWishlistFinder.audit;public class AuditAwareImpl {
+package com.wigner.BoardGameWishlistFinder.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
 }
