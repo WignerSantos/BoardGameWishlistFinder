3f90f397c1e06b22fa74b251bf2262207bd771f6
WignerSantos <wigner12@gmail.com>
2023-10-16 15:42:51 -0300

    Adding Boardgame controller

--- BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/controller/BoardgameController.java
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/controller/BoardgameController.java
@@ -1,2 +1,32 @@
-package com.wigner.BoardGameWishlistFinder.controller;public class BoardgameController {
+package com.wigner.BoardGameWishlistFinder.controller;
+
+import com.wigner.BoardGameWishlistFinder.model.Boardgame;
+import com.wigner.BoardGameWishlistFinder.repositories.BoardgameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class BoardgameController {
+
+    @Autowired
+    BoardgameRepository boardgameRepository;
+
+    @RequestMapping(value = "/boardgames", method = RequestMethod.GET)
+    public ModelAndView displayBoardgamesPage() {
+
+        ModelAndView modelAndView = new ModelAndView("boardgames.html");
+
+        List<Boardgame> boardgameList = boardgameRepository.findAll();
+
+        modelAndView.addObject("boardgames", boardgameList);
+
+        return modelAndView;
+
+    }
+
 }
