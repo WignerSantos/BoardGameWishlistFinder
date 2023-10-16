dacbb76ac70fb5ee65c045c0ca6fb81263097275
WignerSantos <wigner12@gmail.com>
2023-10-16 15:44:45 -0300

    Adding Boardgame repository

--- BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/repositories/BoardgameRepository.java
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/repositories/BoardgameRepository.java
@@ -1,2 +1,12 @@
-package com.wigner.BoardGameWishlistFinder.repositories;public class BoardgameRepository {
+package com.wigner.BoardGameWishlistFinder.repositories;
+
+import com.wigner.BoardGameWishlistFinder.model.Boardgame;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BoardgameRepository extends JpaRepository<Boardgame, Integer> {
+
 }
