0aa40de07bfce3683cafd188fff90adcedfea25d
WignerSantos <wigner12@gmail.com>
2023-10-23 18:50:34 -0300

    Adding create board game function

--- BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/controller/BoardgameController.java
+++ BoardGameWishlistFinder/src/main/java/com/wigner/BoardGameWishlistFinder/controller/BoardgameController.java
@@ -2,13 +2,24 @@ package com.wigner.BoardGameWishlistFinder.controller;
 
 import com.wigner.BoardGameWishlistFinder.model.Boardgame;
 import com.wigner.BoardGameWishlistFinder.repositories.BoardgameRepository;
+import com.wigner.BoardGameWishlistFinder.services.BoardgameService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 
 @Controller
@@ -16,6 +27,9 @@ public class BoardgameController {
 
     @Autowired
     BoardgameRepository boardgameRepository;
+
+    @Autowired
+    BoardgameService boardgameService;
 
     @RequestMapping(value = "/boardgames", method = RequestMethod.GET)
     public ModelAndView displayBoardgamesPage() {
@@ -35,6 +49,29 @@ public class BoardgameController {
         ModelAndView modelAndView = new ModelAndView("create_boardgames.html");
 
         modelAndView.addObject("boardgame", new Boardgame());
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/admin/createBoardgame", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ModelAndView createBoardgame(@Valid @ModelAttribute("boardgame") Boardgame boardgame,
+//                                        @RequestPart(value = "file") MultipartFile file,
+                                        Errors errors) {
+
+        ModelAndView modelAndView = new ModelAndView("create_boardgames.html");
+
+        if(errors.hasErrors()) {
+            return modelAndView;
+        }
+
+        int isSaved = boardgameService.saveBoardgame(boardgame);
+        if(isSaved == 1) {
+            modelAndView.setViewName("redirect:/boardgames");
+        } else if(isSaved == 0) {
+            errors.rejectValue("Name", "duplicate", "Boardgame already exists!");
+        } else if(isSaved == 2) {
+            errors.rejectValue("imageAddress", "null", "Choose an image!");
+        }
 
         return modelAndView;
     }
